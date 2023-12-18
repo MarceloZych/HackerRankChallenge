9277b9c1231909caecb925ea18e613a19ddade93
MarceloZych <74663103+MarceloZych@users.noreply.github.com>
2023-12-18 11:13:35 -0300

    challenge completado 18-12

--- src/main/java/my/challenge/HackerRank/TheLoveLetterMyster/TheLoveLetterMystery.java
+++ src/main/java/my/challenge/HackerRank/TheLoveLetterMyster/TheLoveLetterMystery.java
@@ -28,7 +28,7 @@ class TheLoveLetterMystery {
 
 }
 
-public class Solution {
+class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
